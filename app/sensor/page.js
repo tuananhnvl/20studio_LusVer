a834282a622c8aa955a4096d20b3ceb7f46987c2
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-10-16 20:12:29 +0700

    up

--- app/sensor/page.js
+++ app/sensor/page.js
@@ -21,7 +21,13 @@ export default function page() {
         document.getElementById('accelerationIncludingGravityX').innerHTML = `${take_decimal_number(event.accelerationIncludingGravity.x,4)}`
         document.getElementById('accelerationIncludingGravityY').innerHTML = `${take_decimal_number(event.accelerationIncludingGravity.y,4)}`
     });
-
+    window.addEventListener("deviceorientation", (event) => {
+      // console.log(event)
+       document.getElementById('zA').innerHTML = `${event.absolute}`
+       document.getElementById('xA').innerHTML = `${event.alpha}`
+       document.getElementById('yA').innerHTML = `${event.beta}`
+      
+   });
     function anime() {
       //console.log(obj.current)
 
@@ -37,11 +43,23 @@ export default function page() {
    },[])
    
   return (
-    <div>
+    <div> 
+
+           
+      <div>zA::<h1 id='zA'></h1></div>
+      <div>xA::<h1 id='xA'></h1></div>
+      <div>yA::<h1 id='yA'></h1></div> 
+
+       
+
       <div>accelerationX::<h1 id='accelerationX'></h1></div>
       <div>accelerationY::<h1 id='accelerationY'></h1></div>
       <div>accelerationIncludingGravityX::<h1 id='accelerationIncludingGravityX'></h1></div>
-      <div>accelerationIncludingGravityY::<h1 id='accelerationIncludingGravityY'></h1></div>
+      <div>accelerationIncludingGravityY::<h1 id='accelerationIncludingGravityY'></h1></div> 
+
+
+
+  
     </div>
   )
 }
